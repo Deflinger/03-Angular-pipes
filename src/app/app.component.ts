5998a48ec8140dd083f2922462f644eb60821669
unknown <cesaralejandrovirgenherrera@gmail.com>
2020-08-04 18:10:25 -0500

    Monedas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,7 @@ export class AppComponent {
   
   nombre:string = "Capitan America";
   arreglo=[1,2,3,4,5,6,7,8,9,10];
-  PI: number= Math.PI
+  PI: number = Math.PI
+  porcentaje:number = 0.234;
+  salario:number = 1234.5;
 }
