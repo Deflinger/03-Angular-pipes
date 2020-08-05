8cb3bcd7f42b18c9d5c764d35022fbc79f118e90
unknown <cesaralejandrovirgenherrera@gmail.com>
2020-08-04 21:35:12 -0500

    changing language with buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  nombre:string = "Capitan America";
-  arreglo=[1,2,3,4,5,6,7,8,9,10];
+  nombre:string = 'Capitan America';
+  arreglo= [1,2,3,4,5,6,7,8,9,10];
   PI: number = Math.PI
-  porcentaje:number = 0.234;
-  salario:number = 1234.5;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha:Date= new Date();
+
+  idioma: string = 'fr';
+
+  idioma1: string = '';
+
+  valorPromesa = new Promise<string>((resolve) => {
+
+    setTimeout(() => {
+    resolve('llego la data');
+    }, 2500);
+
+  });
+
+  verFecha(lang: string){
+    return this.idioma1 = lang;
+
+  }
+
+
+  heroe={
+    nombreH:'Logan',
+    clave:'Wolverine',
+    edad: 500,
+    direccion:{
+      calle:'Primera',
+      casa:'20'
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+
+
+//lthe second parameter
+registerLocaleData(localEs);
+registerLocaleData(localFr);
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule ],
+  declarations: [ AppComponent ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+              useValue: 'es'
+    }
+   ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
